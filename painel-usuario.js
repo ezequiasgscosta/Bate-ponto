7b4c4591f510f84d563dc3f65fa45288c23f5dbb
ezequias costa <bieldotor@gmail.com>
2025-10-01 01:05:02 -0300

    ultimo ponto feito

--- painel-usuario.js
+++ painel-usuario.js
@@ -25,3 +25,22 @@ function atualizarDataHora() {
 // Atualiza a cada minuto
 setInterval(atualizarDataHora, 1000);
 atualizarDataHora();
+
+//-------------------Funçoes aqui ----------------
+
+//------------ ultimo ponto batido ---------------
+
+function atualiza_ultimo_ponto() {
+  const ultimo_ponto = document.getElementById("ultimo")
+  
+  const hoje = new Date();
+
+    const dia = hoje.getDate().toString().padStart(2, "0");  
+  const mes = (hoje.getMonth() + 1).toString().padStart(2, "0");
+  const ano = hoje.getFullYear();
+  const horas = hoje.getHours().toString().padStart(2, "0");
+  const minutos = hoje.getMinutes().toString().padStart(2, "0");
+  const hora = `${horas}:${minutos}`;
+
+  ultimo_ponto.textContent = `Ultimo ponto batido: dia ${dia}/${mes}/${ano}  ${hora} `
+}
